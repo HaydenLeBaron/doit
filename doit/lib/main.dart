47d12f934eec87cfc6fb91903232f5a8a3a6d8b5
Hayden LeBaron <hlebaron98@gmail.com>
2021-02-13 14:02:32 -0700

    Refactor main.dart

--- doit/lib/main.dart
+++ doit/lib/main.dart
@@ -1,69 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:doit/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
+import 'package:doit/screens/home_screen.dart';
+import 'package:doit/screens/loading_screen.dart';
+import 'package:doit/screens/something_went_wrong_screen.dart';
 
 void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return SomethingWentWrongScreen();
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return DoitApp();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return LoadingScreen();
-      },
-    );
-  }
-}
-
-class SomethingWentWrongScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Something went wrong..."));
-  }
-}
-
-class LoadingScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text("App Loading..."));
-  }
+  //TestWidgetsFlutterBinding.ensureInitialized()
+  runApp(DoitApp());
 }
 
 class DoitApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Do It',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primaryColor: Colors.blueGrey,
-          accentColor: Colors.white,
-        ),
-        home: HomeScreen());
+      title: 'Do It',
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+        accentColor: Colors.white,
+      ),
+      home: FutureBuilder(
+        // Initialize FlutterFire before anything else
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return SomethingWentWrongScreen();
+          }
+          // Once complete, start to app
+          else if (snapshot.connectionState == ConnectionState.done) {
+            return HomeScreen();
+          }
+          // Otherwise, show something while waiting for initialization to complete
+          else {
+            return LoadingScreen();
+          }
+        },
+      ),
+    );
   }
 }
---
+++ doit/lib/screens/something_went_wrong_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SomethingWentWrongScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Something went wrong!"),
+      backgroundColor: Colors.red,
+    );
+  }
+}
